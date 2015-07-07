386a66a0a15e8edf8e95450e438e354b3f3d204e
alien <18983679028@163.com>
2015-07-07 09:21:43 +0800

    试试

--- app/src/main/java/com/ant/jobgod/jobgod/module/setting/SettingPresenter.java
+++ app/src/main/java/com/ant/jobgod/jobgod/module/setting/SettingPresenter.java
@@ -3,10 +3,15 @@ package com.ant.jobgod.jobgod.module.setting;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.ant.jobgod.jobgod.app.BasePresenter;
 import com.ant.jobgod.jobgod.module.launch.UserLoginActivity;
 import com.ant.jobgod.jobgod.util.Utils;
+import com.umeng.update.UmengUpdateAgent;
+import com.umeng.update.UmengUpdateListener;
+import com.umeng.update.UpdateResponse;
+import com.umeng.update.UpdateStatus;
 
 /**
  * Created by Mr.Jude on 2015/7/6.
@@ -21,7 +26,28 @@ public class SettingPresenter extends BasePresenter<SettingActivity> {
                 new SettingItem("修改密码","通过手机号验证",null),
                 new SettingItem("应用设置"),
                 new SettingItem("意见反馈","",null),
-                new SettingItem("检查更新","",null),
+                new SettingItem("检查更新","",(View v)->{
+                    UmengUpdateAgent.setUpdateAutoPopup(false);
+                    UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
+                        @Override
+                        public void onUpdateReturned(int updateStatus,UpdateResponse updateInfo) {
+                            switch (updateStatus) {
+                                case UpdateStatus.Yes: // has update
+                                    break;
+                                case UpdateStatus.No: // has no update
+                                    Utils.Toast("没有更新");
+                                    break;
+                                case UpdateStatus.NoneWifi: // none wifi
+                                    Utils.Toast("没有wifi连接， 只在wifi下更新");
+                                    break;
+                                case UpdateStatus.Timeout: // time out
+                                    Utils.Toast("超时");
+                                    break;
+                            }
+                        }
+                    });
+                    UmengUpdateAgent.update(getView());
+                }),
                 new SettingItem("关于","",null),
                 new SettingItem("调试设置"),
                 new SettingItem("调试按钮","",null),
