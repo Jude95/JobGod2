b9f54ef40b84e2716b702553a401c5413f51a978
alien <18983679028@163.com>
2015-07-08 21:05:55 +0800

    主页面

---
+++ app/src/main/java/com/ant/jobgod/jobgod/module/job/JobBriefDetailPresenter.java
@@ -0,0 +1,21 @@
+package com.ant.jobgod.jobgod.module.job;
+
+import android.os.Bundle;
+
+import com.ant.jobgod.jobgod.app.BasePresenter;
+
+/**
+ * Created by alien on 2015/7/8.
+ */
+public class JobBriefDetailPresenter extends BasePresenter<JobBriefDetailActivity> {
+
+    @Override
+    protected void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+    }
+
+    @Override
+    protected void onCreateView(JobBriefDetailActivity view) {
+        super.onCreateView(view);
+    }
+}
