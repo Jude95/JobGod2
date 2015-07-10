8657a94cf6dbcdd3f488d6a6f19b26f156e35525
alien <18983679028@163.com>
2015-07-10 10:57:37 +0800

    Merge branch 'master' of https://git.oschina.net/jude95/JobGod
    Conflicts:
    	app/src/main/java/com/ant/jobgod/jobgod/module/job/JobDetailPresenter.java

--- app/src/main/java/com/ant/jobgod/jobgod/module/user/UserDetailActivity.java
+++ app/src/main/java/com/ant/jobgod/jobgod/module/user/UserDetailActivity.java
@@ -1,11 +1,14 @@
 package com.ant.jobgod.jobgod.module.user;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 import com.ant.jobgod.jobgod.R;
+import com.ant.jobgod.jobgod.app.BaseActivity;
 
-public class UserDetailActivity extends ActionBarActivity {
+import nucleus.factory.RequiresPresenter;
+
+@RequiresPresenter(UserDetailPresenter.class)
+public class UserDetailActivity extends BaseActivity<UserDetailPresenter> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/ant/jobgod/jobgod/module/user/UserDetailPresenter.java
@@ -0,0 +1,9 @@
+package com.ant.jobgod.jobgod.module.user;
+
+import com.ant.jobgod.jobgod.app.BasePresenter;
+
+/**
+ * Created by alien on 2015/7/10.
+ */
+public class UserDetailPresenter extends BasePresenter<UserDetailActivity> {
+}
