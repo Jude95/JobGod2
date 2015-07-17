768b78f17e77e6b87082d7dc5e2fece42ea8a169
Jude95 <973829691@qq.com>
2015-07-17 20:40:45 +0800

    破解64K方法上限

--- app/src/main/java/com/ant/jobgod/jobgod/module/user/ModifyFacePresenter.java
+++ app/src/main/java/com/ant/jobgod/jobgod/module/user/ModifyFacePresenter.java
@@ -40,6 +40,7 @@ public class ModifyFacePresenter extends BasePresenter<ModifyFaceActivity> {
     protected void onCreate(Bundle savedState) {
         super.onCreate(savedState);
         mProvider = new ImageProvider(getView());
+        Utils.Log("onCreate");
         //getView().setImgFace(Uri.parse(AccountModel.getInstance().getAccount().getFace()));
     }
 
@@ -92,6 +93,7 @@ public class ModifyFacePresenter extends BasePresenter<ModifyFaceActivity> {
 
     public void startCrop(Uri data){
         //删除上一次的图片
+        if (mFinalImg != null)
         FileManager.getInstance().deletChild(FileManager.Dir.Image,mFinalImg);
         //用时间来取名，临时措施
         mFinalImg = System.currentTimeMillis()+".jpg";
