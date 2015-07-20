ab04610090fe0b149ae2e984de1052f5f615ff8b
alien <18983679028@163.com>
2015-07-20 15:32:03 +0800

    Merge branch 'master' of https://git.oschina.net/jude95/JobGod
    Conflicts:
    	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/ant/jobgod/jobgod/module/job/DiscussPresenter.java
@@ -0,0 +1,20 @@
+package com.ant.jobgod.jobgod.module.job;
+
+import android.os.Bundle;
+
+import com.ant.jobgod.jobgod.app.BasePresenter;
+
+/**
+ * Created by alien on 2015/7/20.
+ */
+public class DiscussPresenter extends BasePresenter<DiscussActivity> {
+    @Override
+    protected void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+    }
+
+    @Override
+    protected void onCreateView(DiscussActivity view) {
+        super.onCreateView(view);
+    }
+}
